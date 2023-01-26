e27377fd358a8c6f29ecd2303d001958cafb2506
betocostadev <robertomarx@gmail.com>
2023-01-26 18:30:32 -0300

    adding react router dom

--- hungryman/src/index.tsx
+++ hungryman/src/index.tsx
@@ -1,13 +1,11 @@
-import Home from 'pages/Home'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import AppRouter from 'router'
 import './index.css'
-import Menu from './pages/Menu'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <Home />
-    {/* <Menu /> */}
+    <AppRouter />
   </React.StrictMode>
 )
