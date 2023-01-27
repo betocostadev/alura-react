d9f29bae3d61884e819208c2823a77afa55ca76d
betocostadev <robertomarx@gmail.com>
2023-01-27 19:34:54 -0300

    improved notFound page

--- hungryman/src/pages/NotFound/index.tsx
+++ hungryman/src/pages/NotFound/index.tsx
@@ -2,8 +2,14 @@ import { ReactComponent as NotFoundImage } from 'assets/not_found.svg'
 import classNames from 'classnames'
 import styles from './NotFound.module.scss'
 import stylesTheme from 'styles/Theme.module.scss'
+import { useNavigate } from 'react-router-dom'
 
 const NotFound = () => {
+  const navigate = useNavigate()
+
+  const goBack = () => {
+    navigate(-1)
+  }
   return (
     <div
       className={classNames({
@@ -11,9 +17,12 @@ const NotFound = () => {
         [stylesTheme.container]: true,
       })}
     >
+      <div className={styles.goBack}>
+        <button onClick={() => goBack()}>{'< Voltar'}</button>
+      </div>
       <NotFoundImage />
       <h3>OOOoooops.... 404</h3>
-      <h6>Looks like the page you are trying to access doesn&apos;t exist</h6>
+      <h6>Looks like the page you are trying to access doesn&apos;t exist.</h6>
     </div>
   )
 }
